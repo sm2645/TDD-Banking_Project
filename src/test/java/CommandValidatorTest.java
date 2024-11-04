286ad839fba909989b012e8a67795aa1b99959fc
Shone <sm4872@drexel.edu>
2024-11-03 22:34:43 -0500

    Made ComandValidator test file and the validate test command.

---
+++ src/test/java/CommandValidatorTest.java
@@ -0,0 +1,21 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CommandValidatorTest {
+	CommandValidator commandValidator;
+	Bank bank;
+
+	@BeforeEach
+	void setUp() {
+		bank = new Bank();
+		commandValidator = new CommandValidator(bank);
+	}
+
+	@Test
+	void valid_command() {
+		boolean actual = commandValidator.validate("create savings 12345678 0.5");
+		assertTrue(actual);
+	}
+}
