daeb885b917fc30835de426bc2e9e08d492f793b
Shone <sm4872@drexel.edu>
2024-11-13 20:38:57 -0500

    Made DepositCommandProcessor according to project directions

--- src/main/java/DepositCommandProcessor.java
+++ src/main/java/DepositCommandProcessor.java
@@ -1,4 +1,14 @@
 public class DepositCommandProcessor {
+	private final Bank bank;
+
 	public DepositCommandProcessor(Bank bank) {
+		this.bank = bank;
+	}
+
+	public void process(String[] commandSeparated) {
+		String accountId = commandSeparated[1];
+		double depositAmount = Double.parseDouble(commandSeparated[2]);
+
+		bank.retrieveAccount(accountId).deposit(depositAmount);
 	}
 }
