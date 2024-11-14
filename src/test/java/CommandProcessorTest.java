4b27ea629aec33759d1d5f10497918bba2076f17
Shone <sm4872@drexel.edu>
2024-11-13 21:10:22 -0500

    Made DepositCommandProcessorTests according to TDD and project directions to test needed cases

--- src/test/java/CommandProcessorTest.java
+++ src/test/java/CommandProcessorTest.java
@@ -15,16 +15,16 @@ public class CommandProcessorTest {
 
 	@Test
 	public void testCreateCommand() {
-		commandProcessor.processCommand("create checking 12345678 1.0");
+		commandProcessor.process("create checking 12345678 1.0");
 		Accounts account = bank.retrieveAccount("12345678");
 		assertEquals(1.0, account.getAPR());
 	}
 
 	@Test
 	public void testDepositCommand() {
-		commandProcessor.processCommand("create checking 12345678 1.0");
+		commandProcessor.process("create checking 12345678 1.0");
 
-		commandProcessor.processCommand("deposit 12345678 100");
+		commandProcessor.process("deposit 12345678 100");
 		Accounts account = bank.retrieveAccount("12345678");
 		assertEquals(100.0, account.getBalance());
 	}
---
+++ src/test/java/DepositCommandProcessorTest.java
@@ -0,0 +1,91 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DepositCommandProcessorTest {
+	public static final String ACCOUNT_ID = "01234567";
+	public static final double APR = 1;
+	public static final double BALANCE = 0;
+	public static final double DEPOSIT = 500;
+
+	private Bank bank;
+	private CommandProcessor commandProcessor;
+
+	@BeforeEach
+	void setUp() {
+		bank = new Bank();
+		commandProcessor = new CommandProcessor(bank);
+	}
+
+//Checking
+	@Test
+	public void depositing_zero_works_for_checking_account() {
+		bank.addAccount(ACCOUNT_ID, new Checking(ACCOUNT_ID, APR));
+
+		commandProcessor.process("deposit 01234567 0");
+		assertEquals(BALANCE, bank.retrieveAccount("01234567").getBalance());
+	}
+
+	@Test
+	public void deposit_works_for_checking_account() {
+		bank.addAccount(ACCOUNT_ID, new Checking(ACCOUNT_ID, APR));
+
+		commandProcessor.process("deposit 01234567 500");
+		assertEquals(DEPOSIT, bank.retrieveAccount("01234567").getBalance());
+	}
+
+	@Test
+	public void deposit_works_for_max_limit_of_checking_account() {
+		bank.addAccount(ACCOUNT_ID, new Checking(ACCOUNT_ID, APR));
+
+		commandProcessor.process("deposit 01234567 1000");
+		assertEquals(1000, bank.retrieveAccount("01234567").getBalance());
+	}
+
+	@Test
+	public void deposit_works_multiple_times_for_checking_account() {
+		bank.addAccount(ACCOUNT_ID, new Checking(ACCOUNT_ID, APR));
+
+		commandProcessor.process("deposit 01234567 500");
+		commandProcessor.process("deposit 01234567 500");
+		commandProcessor.process("deposit 01234567 500");
+		assertEquals(3 * DEPOSIT, bank.retrieveAccount("01234567").getBalance());
+	}
+
+//Savings
+	@Test
+	public void depositing_zero_works_for_savings_account() {
+		bank.addAccount(ACCOUNT_ID, new Savings(ACCOUNT_ID, APR));
+
+		commandProcessor.process("deposit 01234567 0");
+		assertEquals(BALANCE, bank.retrieveAccount("01234567").getBalance());
+	}
+
+	@Test
+	public void deposit_works_for_savings_account() {
+		bank.addAccount(ACCOUNT_ID, new Savings(ACCOUNT_ID, APR));
+
+		commandProcessor.process("deposit 01234567 500");
+		assertEquals(DEPOSIT, bank.retrieveAccount("01234567").getBalance());
+	}
+
+	@Test
+	public void deposit_works_for_max_limit_of_savings_account() {
+		bank.addAccount(ACCOUNT_ID, new Savings(ACCOUNT_ID, APR));
+
+		commandProcessor.process("deposit 01234567 2500");
+		assertEquals(2500, bank.retrieveAccount("01234567").getBalance());
+	}
+
+	@Test
+	public void deposit_works_multiple_times_for_savings_account() {
+		bank.addAccount(ACCOUNT_ID, new Savings(ACCOUNT_ID, APR));
+
+		commandProcessor.process("deposit 01234567 500");
+		commandProcessor.process("deposit 01234567 500");
+		commandProcessor.process("deposit 01234567 500");
+		assertEquals(3 * DEPOSIT, bank.retrieveAccount("01234567").getBalance());
+	}
+
+}
