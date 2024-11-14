82fa3553613fe1c7174662c4fe551114d7753f8b
Shone <sm4872@drexel.edu>
2024-11-13 23:36:35 -0500

    Used TDD to make MasterControl from MasterControlTest

--- src/test/java/CommandStorageTest.java
+++ src/test/java/CommandStorageTest.java
@@ -1,6 +1,9 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -17,7 +20,7 @@ public class CommandStorageTest {
 	void invalid_tests_can_be_added_to_command_storage() {
 
 		commandStorage.addInvalidCommand(INVALID_COMMAND);
-		assertTrue(commandStorage.getInvalidCommands().contains(INVALID_COMMAND));
+		assertEquals(INVALID_COMMAND, commandStorage.getInvalidCommands().get(0));
 	}
 
 	@Test
@@ -29,7 +32,30 @@ public class CommandStorageTest {
 	}
 
 	@Test
+	void multiple_diffrent_invalid_tests_are_all_stored_in_command_storage() {
+		commandStorage.addInvalidCommand("undeposit abcdedfs -500");
+		commandStorage.addInvalidCommand("undeposits acsbcdedfs -500");
+
+		String expected = "undeposit abcdedfs -500 undeposits acsbcdedfs -500";
+		String actual = String.join(" ", commandStorage.getInvalidCommands());
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
 	void getInvalidCommands_returns_empty_list_when_no_commands_added() {
 		assertTrue(commandStorage.getInvalidCommands().isEmpty());
+	}
+
+	@Test
+	void getInvalidCommands_returns_list_with_every_invalid_command_added() {
+		commandStorage.addInvalidCommand(INVALID_COMMAND);
+		commandStorage.addInvalidCommand(INVALID_COMMAND);
+		commandStorage.addInvalidCommand("INVALID_COMMAND");
+		commandStorage.addInvalidCommand("INVALIDCOMMAND");
+		ArrayList<String> invalidCommandsList = new ArrayList<>(
+				Arrays.asList(INVALID_COMMAND, INVALID_COMMAND, "INVALID_COMMAND", "INVALIDCOMMAND"));
+
+		assertEquals(invalidCommandsList, commandStorage.getInvalidCommands());
 	}
 }
--- src/test/java/MasterControlTest.java
+++ src/test/java/MasterControlTest.java
@@ -1,3 +1,67 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class MasterControlTest {
+	MasterControl masterControl;
+	List<String> input;
+
+	@BeforeEach
+	void setUp() {
+		input = new ArrayList<>();
+		Bank bank = new Bank();
+		CommandValidator commandValidator = new CommandValidator(bank, null, null);
+		CreateValidator createValidator = new CreateValidator(bank, commandValidator);
+		DepositValidator depositValidator = new DepositValidator(bank, commandValidator);
+		masterControl = new MasterControl(new CommandValidator(bank, createValidator, depositValidator),
+				new CommandProcessor(bank), new CommandStorage());
+	}
+
+	@Test
+	void typo_in_create_command_is_invalid() {
+		List<String> inputs = new ArrayList<>();
+		input.add("creat checking 12345678 1.0");
+
+		List<String> actual = masterControl.start(input);
+		assertEquals(1, actual.size());
+		assertSingleCommand("creat checking 12345678 1.0", actual);
+	}
+
+	@Test
+	void typo_in_deposit_command_is_invalid() {
+		input.add("depositt 12345678 100");
+
+		List<String> actual = masterControl.start(input);
+
+		assertSingleCommand("depositt 12345678 100", actual);
+	}
+
+	@Test
+	void two_typo_commands_both_invalid() {
+		input.add("creat checking 12345678 1.0");
+		input.add("depositt 12345678 100");
+		List<String> actual = masterControl.start(input);
+
+		assertEquals(2, actual.size());
+		assertEquals("creat checking 12345678 1.0", actual.get(0));
+		assertEquals("depositt 12345678 100", actual.get(1));
+	}
+
+	@Test
+	void invalid_to_create_accounts_with_same_ID() {
+		input.add("create checking 12345678 1.0");
+		input.add("create checking 12345678 1.0");
+		List<String> actual = masterControl.start(input);
+		assertSingleCommand("create checking 12345678 1.0", actual);
+	}
+
+	private void assertSingleCommand(String command, List<String> actual) {
+		assertEquals(1, actual.size());
+		assertEquals(command, actual.get(0));
+	}
 
 }
