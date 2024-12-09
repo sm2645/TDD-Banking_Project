a7616166939808f5f74a640af29efa184acc3af1
Shone <sm4872@drexel.edu>
2024-12-08 21:28:03 -0500

    made change according to CQ_html

--- src/main/java/banking/CreateValidator.java
+++ src/main/java/banking/CreateValidator.java
@@ -7,7 +7,6 @@ public class CreateValidator {
 
 	public CreateValidator(Bank bank) {
 		this.bank = bank;
-
 	}
 
 	public boolean validate(String[] commandSeparated) {
@@ -18,32 +17,37 @@ public class CreateValidator {
 		String accountType = commandSeparated[1];
 		String accountId = commandSeparated[2];
 		String apr = commandSeparated[3];
-		String balance = null;
 
 		if (bank.ifAccountExists(accountId)) {
 			return false;
 		}
 
 		if (Objects.equals(accountType, "cd")) {
-			if (commandSeparated.length != 5) {
-				return false;
-			}
-			balance = commandSeparated[4];
-		}
-		if (Objects.equals(accountType, "cd")) {
-			return bank.isValidAccountId(accountId) && bank.isValidApr(apr) && bank.isValidAmount(balance)
-					&& (Double.parseDouble(balance) >= 1000 && Double.parseDouble(balance) <= 10000);
+			return validateCdAccount(commandSeparated, accountId, apr);
 		} else {
-			if (commandSeparated.length != 4) {
-				return false;
-			}
-			return isValidAccountType(accountType) && bank.isValidAccountId(accountId) && bank.isValidApr(apr);
+			return validateNonCdAccount(commandSeparated, accountType, accountId, apr);
+		}
+	}
+
+	private boolean validateCdAccount(String[] commandSeparated, String accountId, String apr) {
+		if (commandSeparated.length != 5) {
+			return false;
 		}
 
+		String balance = commandSeparated[4];
+		return bank.isValidAccountId(accountId) && bank.isValidApr(apr) && bank.isValidAmount(balance)
+				&& (Double.parseDouble(balance) >= 1000 && Double.parseDouble(balance) <= 10000);
+	}
+
+	private boolean validateNonCdAccount(String[] commandSeparated, String accountType, String accountId, String apr) {
+		if (commandSeparated.length != 4) {
+			return false;
+		}
+
+		return isValidAccountType(accountType) && bank.isValidAccountId(accountId) && bank.isValidApr(apr);
 	}
 
 	public boolean isValidAccountType(String type) {
 		return Objects.equals(type, "savings") || Objects.equals(type, "checking") || Objects.equals(type, "cd");
 	}
-
 }
